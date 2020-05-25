e9d53631004beaba963bb349718372c4e2f005ff
Jan Fischlmayr <jan.fischlmayr@gmx.net>
2020-05-25 09:44:56 +0200

    Dash Test works

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
 
-    private static final String PATTERN = "\\d{2}\\.\\d{2}\\.\\d{4}";
+    private static final String PATTERN = "\\d{2}[.|-]\\d{2}[.|-]\\d{4}";
     private static Pattern pattern = Pattern.compile(PATTERN);
 
     public static Matcher getMatcher(String dateString) {
